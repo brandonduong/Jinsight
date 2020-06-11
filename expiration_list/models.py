1cfc062c16ede996e7d2ea6ea65a0cff31b707a9
Brandon Duong <brandon.duong@outlook.com>
2020-06-11 19:42:30 -0400

    small improvements

--- expiration_list/models.py
+++ expiration_list/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class ExpirationList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="expirationlist", null=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    expiration_list = models.ForeignKey(ExpirationList, on_delete=models.CASCADE)
+    text = models.CharField(max_length=50)
+    good = models.BooleanField()
+
+    def __str__(self):
+        return self.text
--- expiration_list/views.py
+++ expiration_list/views.py
@@ -5,3 +5,4 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'expiration_list/index.html', context)
+
